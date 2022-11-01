1068c34aaf34b8018ba4db6bbcfbf954d2bd6ca7
Marc-Philippe Fuller <marc-philippe.fuller@intel.com>
2022-11-01 11:09:21 -0700

    fix: updated copyright years and dockerfile
    Signed-off-by: Marc-Philippe Fuller <marc-philippe.fuller@intel.com>

--- rtsf-at-checkout-loss-detector/functions/notify.go
+++ rtsf-at-checkout-loss-detector/functions/notify.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 Intel Corporation. All rights reserved.
+// Copyright © 2022 Intel Corporation. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
 package functions
